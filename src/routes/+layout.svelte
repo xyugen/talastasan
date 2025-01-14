25d826e494093bab4728b1e55e5ba519be182d62
xyugen <alex.arias.jorym@gmail.com>
2025-01-14 19:46:02 +0800

    feat: initialize svelte-ux

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
-<script lang="ts">
+<script>
 	import '../app.css';
 	import Logo from '$lib/assets/logo.png';
+	import { Button, Menu, MenuItem, Toggle } from 'svelte-ux';
 	let { children } = $props();
-
-	let x = ["a", "b", "c"];
 </script>
 
 <svelte:head>
 	<title>TalasTasan</title>
 </svelte:head>
 
-<header class="p-4 flex flex-row items-center justify-between">
+<header class="bg bg-secondary text-primary-content flex flex-row items-center justify-between p-4">
 	<a href="/" class="flex w-fit flex-row items-center gap-2">
 		<img src={Logo} alt="logo" class="w-12" />
 		<h1 class="text-lg font-bold">TalasTasan</h1>
@@ -18,7 +17,19 @@
 
 	<nav class="flex flex-row items-center gap-4">
 		<a href="/" class="btn btn-ghost">Home</a>
-		<a href="/" class="btn btn-ghost">Home</a>
+		<Toggle let:on={open} let:toggle let:toggleOff>
+			<Button on:click={toggle}>
+				Click me
+				<Menu {open} on:close={toggleOff}>
+					<MenuItem>Refresh</MenuItem>
+					<MenuItem>Settings</MenuItem>
+					<MenuItem>Help</MenuItem>
+					<MenuItem>Sign In</MenuItem>
+					<MenuItem disabled>Disabled</MenuItem>
+				</Menu>
+			</Button>
+		</Toggle>
+
 		<a href="/" class="btn btn-ghost">Home</a>
 		<a href="/about" class="btn btn-ghost">About</a>
 	</nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,44 @@
 import type { Config } from 'tailwindcss';
+import svelteUx from 'svelte-ux/plugins/tailwind.cjs';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/svelte-ux/**/*.{svelte,js}'],
 
 	theme: {
 		extend: {
 			fontFamily: {
 				poppins: ['Poppins', 'sans-serif'],
-				lora: ['Lora', 'serif'],
+				lora: ['Lora', 'serif']
 			}
 		}
 	},
 
-	plugins: []
+	ux: {
+		themes: {
+			light: {
+				'color-scheme': 'light',
+				primary: 'hsl(359 39% 33%)',
+				secondary: 'hsl(0 0% 11%)',
+				accent: 'hsl(36 32% 58%)',
+				neutral: 'hsl(22.1053 17.1171% 43.5294%)',
+				info: 'hsl(186.9421 47.8261% 49.6078%)',
+				success: 'hsl(164.5946 33.6364% 43.1373%)',
+				warning: 'hsl(30.1408 84.1897% 49.6078%)',
+				danger: 'hsl(354.4845 100% 41.642%)',
+				'surface-100': 'hsl(0 0% 94.5098%)'
+			},
+			// dark: {
+			// 	'color-scheme': 'dark',
+			// 	primary: 'hsl(234.8208 100% 72.6713%)',
+			// 	secondary: 'hsl(313.3209 100% 66.1653%)',
+			// 	accent: 'hsl(173.7346 100% 40.1728%)',
+			// 	neutral: 'hsl(213.3333 17.6471% 20%)',
+			// 	'surface-100': 'hsl(212.3077 18.3099% 13.9216%)',
+			// 	'surface-200': 'hsl(212.7273 18.0328% 11.9608%)',
+			// 	'surface-300': 'hsl(213.3333 17.6471% 10%)'
+			// }
+		}
+	},
+
+	plugins: [svelteUx]
 } satisfies Config;

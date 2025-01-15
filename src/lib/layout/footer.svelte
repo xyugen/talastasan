0bad62aef764b9781a5e9502b5843b18985bdb7c
xyugen <alex.arias.jorym@gmail.com>
2025-01-15 18:28:06 +0800

    feat: remove socmed div space in footer

--- src/lib/layout/footer.svelte
+++ src/lib/layout/footer.svelte
@@ -5,18 +5,18 @@
 <footer class="bg-secondary py-8">
 	<div class="container mx-auto px-4">
 		<div class="flex flex-col items-center justify-center space-y-4">
-      <!-- TODO: add social media links -->
-			<div class="flex space-x-6">
+			<!-- TODO: add social media links -->
+			<!-- <div class="flex space-x-6">
 				<a href="#" class="text-gray-400 transition-colors hover:text-white">
-					<!-- <Facebook class="h-6 w-6" /> -->
+					<Facebook class="h-6 w-6" /> 
 				</a>
 				<a href="#" class="text-gray-400 transition-colors hover:text-white">
-					<!-- <Instagram class="h-6 w-6" /> -->
+					<Instagram class="h-6 w-6" /> 
 				</a>
 				<a href="#" class="text-gray-400 transition-colors hover:text-white">
-					<!-- <Twitter class="h-6 w-6" /> -->
+					<Twitter class="h-6 w-6" />
 				</a>
-			</div>
+			</div> -->
 			<p class="text-center text-sm text-secondary-content/60">
 				Copyright &copy; {year}, TalasTasan. All Rights Reserved.
 			</p>

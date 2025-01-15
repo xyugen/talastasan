5e64f424e17758a8a6d4e7ff8a6d28a240d8eb2b
xyugen <alex.arias.jorym@gmail.com>
2025-01-15 18:11:49 +0800

    feat: improve chapter selection style

--- src/lib/components/story-card.svelte
+++ src/lib/components/story-card.svelte
@@ -14,20 +14,19 @@
 	export let href: StoryCardProps['href'];
 </script>
 
-<div
-	class="border-primary/30 group relative overflow-hidden rounded-lg border bg-white transition-transform hover:-translate-y-1"
+<a
+	{href}
+	class="group relative overflow-hidden rounded-lg border border-primary/30 bg-white transition-transform hover:-translate-y-1"
 >
-	<a {href}>
-		<div class="relative aspect-video overflow-hidden">
-			<enhanced:img
-				src={image}
-				alt={title}
-				class="object-cover transition-transform group-hover:scale-100"
-			/>
-		</div>
-		<div class="p-6">
-			<h3 class="mb-3 text-xl font-semibold text-[#2D2522]">{title}</h3>
-			<p class="text-gray-600">{description}</p>
-		</div>
-	</a>
-</div>
+	<div class="relative aspect-video overflow-hidden">
+		<enhanced:img
+			src={image}
+			alt={title}
+			class="object-cover transition-transform group-hover:scale-100"
+		/>
+	</div>
+	<div class="p-6">
+		<h3 class="mb-3 text-xl font-semibold text-[#2D2522]">{title}</h3>
+		<p class="text-gray-600">{description}</p>
+	</div>
+</a>
--- src/routes/buong-kuwento/+page.svelte
+++ src/routes/buong-kuwento/+page.svelte
@@ -11,21 +11,21 @@
 <StoryHeroSection imageCover={BuongKuwentoCover} />
 
 <section class="min-h-screen bg-surface-100 p-8 text-surface-content">
-	<div class="container mx-auto max-w-3xl">
+	<div class="container mx-auto max-w-4xl">
 		<h1 class="mb-6 text-center text-4xl font-semibold">Buong Kuwento</h1>
 		<p class="mb-8 text-center text-lg text-surface-content/80">
 			Piliin ang pahina upang simulan ang iyong pagbabasa.
 		</p>
-		<div class="grid gap-4">
+		<div class="grid md:grid-cols-2 gap-4">
 			{#each fullStoryChapters as chapter}
 				<a
 					href="/buong-kuwento{chapter.link}"
-					class="space-y-2 block rounded-lg border border-primary/5 bg-primary/20 p-4 shadow-md transition hover:bg-primary/15 hover:shadow-lg"
+					class="block space-y-2 rounded-lg border border-primary/30 bg-white p-4 shadow-md transition hover:shadow-lg"
 				>
-					<h2 class="text-xl font-medium text-primary">{chapter.title}</h2>
-					<p class="text-sm text-primary">
-            {chapter.content.slice(0, 200)}...
-          </p>
+					<h3 class="text-xl font-medium text-[#2D2522]">{chapter.title}</h3>
+					<p class="text-sm text-[#2D2522]/80">
+						{chapter.content.slice(0, 200)}...
+					</p>
 				</a>
 			{/each}
 		</div>

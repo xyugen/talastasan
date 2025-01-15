6660ec803823ba152959819855b2e50f99288226
xyugen <alex.arias.jorym@gmail.com>
2025-01-15 18:24:51 +0800

    feat: improve nav menu

--- src/lib/layout/header.svelte
+++ src/lib/layout/header.svelte
@@ -19,13 +19,12 @@
 </script>
 
 <header
-	class="bg-secondary text-primary-content fixed z-50 flex w-full flex-row items-center justify-between p-4 transition-all duration-150"
+	class="fixed z-50 flex w-full flex-row items-center justify-between bg-secondary px-4 py-3 text-primary-content transition-all duration-150 md:px-8"
 	style="--tw-bg-opacity: {isScrolled ? 1 : 0}"
 >
 	<a href="/" class="flex w-fit flex-row items-center gap-2">
 		<enhanced:img src={Logo} alt="logo" class="w-12" />
 		<h1 class="text-lg font-bold">TalasTasan</h1>
 	</a>
-
 	<NavMenu />
 </header>

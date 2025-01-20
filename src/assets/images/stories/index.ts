2710cb3e2eb8065a3b9f3cb66f3bfaec5c018649
xyugen <alex.arias.jorym@gmail.com>
2025-01-20 16:43:52 +0800

    chore: optimize code

---
+++ src/assets/images/stories/index.ts
@@ -0,0 +1,7 @@
+export { default as BuongKuwento } from "./buong-kuwento.png";
+export { default as Buod } from "./buod.png";
+export { default as KuwentongMayLarawan } from "./kuwentong-may-larawan.png";
+
+export { default as BuongKuwentoCover } from "./buong-kuwento-cover.png";
+export { default as BuodCover } from "./buod-cover.png";
+export { default as KuwentongMayLarawanCover } from "./kuwentong-may-larawan-cover.png";
--- src/assets/images/written-works/index.ts
+++ src/assets/images/written-works/index.ts
@@ -1,4 +1,4 @@
-export * as ImpengNegro from './impeng-negro.png';
-export * as TataSeto from './tata-seto.png';
-export * as Pagsalunga from './pagsalunga.png';
-export * as DugoSaBukangLiwayway from './dugo-sa-bukang-liwayway.png';
+export { default as ImpengNegro } from "./impeng-negro.png";
+export { default as TataSeto } from "./tata-seto.png";
+export { default as Pagsalunga } from "./pagsalunga.png";
+export { default as DugoSaBukangLiwayway } from "./dugo-sa-bukang-liwayway.png";

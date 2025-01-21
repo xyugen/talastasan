1df3fa69e1440efc0633b18f76b62389a4d0a520
xyugen <alex.arias.jorym@gmail.com>
2025-01-21 19:28:14 +0800

    feat: add dot pattern on details section

--- src/app/_components/details-section.tsx
+++ src/app/_components/details-section.tsx
@@ -1,10 +1,18 @@
+import { DotPattern } from "@/components/patterns/dot-pattern";
 import BlurFade from "@/components/ui/blur-fade";
+import { cn } from "@/lib/utils";
 import { Quote } from "lucide-react";
 
 const DetailsSection = () => {
   return (
-    <section id="details" className="py-32">
-      <div className="space-y-8 px-6 text-center md:px-20 lg:px-40">
+    <section id="details" className="py-20">
+      <div className="relative space-y-8 px-6 text-center md:px-20 lg:px-40">
+        <DotPattern
+          className={cn(
+            "[mask-image:radial-gradient(600px_circle_at_center,white,transparent)] md:mt-36 mt-24", "fill-primary"
+          )}
+        />
+        
         <BlurFade
           className="relative bg-card border-4 border-secondary rounded-lg p-8 mx-auto w-fit space-y-2"
           delay={0.5}
@@ -16,10 +24,7 @@ const DetailsSection = () => {
           </h3>
         </BlurFade>
 
-        <BlurFade
-        delay={0.65}
-        inView
-        className="text-lg text-foreground">
+        <BlurFade delay={0.65} inView className="text-lg text-foreground">
           Ang TalasTasan ay isang mungkahing website na naglalaman ng mga akdang
           pampanitikan na magsisilbing isang kapaki-pakinabang na kagamitang
           pampagtuturo at pagkatuto para sa mga guro at mag-aaral upang higit

c89613e1e9a2b8ffc22887c436d76fb1a7252a61
xyugen <alex.arias.jorym@gmail.com>
2025-01-21 23:47:20 +0800

    feat: improve SEO and add more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
+import Footer from "@/components/layouts/footer";
+import Header from "@/components/layouts/header";
+import TransitionLoader from "@/components/transition-loader";
 import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "./lib/font";
-import Header from "@/components/layouts/header";
-import Footer from "@/components/layouts/footer";
-import TransitionLoader from "@/components/transition-loader";
 
 export const metadata: Metadata = {
   title: {
@@ -24,6 +24,30 @@ export const metadata: Metadata = {
     { name: "Renz Arias", url: "https://renzyx.is-a.dev" },
     { name: "Pagzone", url: "https://github.com/pagzone" },
   ],
+  openGraph: {
+    title: "TalasTasan",
+    description:
+      "Paglinang ng kasanayan sa pagbasa. Pagpapalawak ng bokabularyo.",
+    url: "https://talastasan.vercel.app",
+    siteName: "TalasTasan",
+    type: "website",
+    images: ["/opengraph.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Josh Daniel",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({

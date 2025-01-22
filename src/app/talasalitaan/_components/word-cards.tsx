54b7af682027a0b139f897e1100bb1c8ed69c906
xyugen <alex.arias.jorym@gmail.com>
2025-01-22 20:58:05 +0800

    feat: sort talasalitaan alphabetically

--- src/app/talasalitaan/_components/word-cards.tsx
+++ src/app/talasalitaan/_components/word-cards.tsx
@@ -7,9 +7,11 @@ import WordCard from './word-card';
 const WordCards = () => {
   return (
     <div className='space-y-4'>
-      {talasalitaan.map((word) => (
-        <WordCard key={word.word} word={word} />
-      ))}
+      {talasalitaan
+        .sort((a, b) => a.word.localeCompare(b.word))
+        .map((word) => (
+          <WordCard key={word.word} word={word} />
+        ))}
     </div>
   );
 }

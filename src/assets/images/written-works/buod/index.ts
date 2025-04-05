0a037766349a971f70a3d1410c586c895498de77
xyugen <alex.arias.jorym@gmail.com>
2025-04-05 11:00:46 +0800

    feat: add images to story covers in published works

---
+++ src/assets/images/written-works/buod/index.ts
@@ -0,0 +1,3 @@
+export { default as QuentinBuodCover } from "./quentin.jpg";
+export { default as SaLupaNgSarilingBayanBuodCover } from "./sa-lupa-ng-sariling-bayan.jpg";
+export { default as TataSeloBuodCover } from "./tata-selo.jpg";
--- src/data/stories.ts
+++ src/data/stories.ts
@@ -3,6 +3,21 @@ import {
   BuongKuwento as BuongKuwentoImg,
   KuwentongMayLarawan as KuwentongMayLarawanImg,
 } from "@/assets/images/stories";
+import {
+  QuentinBuodCover,
+  SaLupaNgSarilingBayanBuodCover,
+  TataSeloBuodCover,
+} from "@/assets/images/written-works/buod";
+import {
+  QuentinBuongKuwentoCover,
+  SaLupaNgSarilingBayanBuongKuwentoCover,
+  TataSeloBuongKuwentoCover,
+} from "@/assets/images/written-works/buong-kuwento";
+import {
+  QuentinKuwentongMayLarawanCover,
+  SaLupaNgSarilingKuwentongMayLarawanCover,
+  TataSeloKuwentongMayLarawanCover,
+} from "@/assets/images/written-works/kuwentong-may-larawan";
 import { StaticImageData } from "next/image";
 
 export interface Story {
@@ -34,6 +49,72 @@ const impengNegroStories: Story[] = [
   },
 ];
 
+const tataSeloStories: Story[] = [
+  {
+    image: TataSeloBuongKuwentoCover,
+    link: "#buong-kuwento",
+    title: "Buong Kuwento",
+    description: "Tingnan ang detalyadong pangyayari.",
+  },
+  {
+    image: TataSeloBuodCover,
+    link: "#buod",
+    title: "Buod",
+    description: "Isang maikling buod ng kwento.",
+  },
+  {
+    image: TataSeloKuwentongMayLarawanCover,
+    link: "#kuwentong-may-larawan",
+    title: "Kuwentong May Larawan",
+    description:
+      "Tingnan ang detalyadong pangyayari sa pamamagitan ng mga larawan.",
+  },
+];
+
+const saLupaNgSarilingBayanStories: Story[] = [
+  {
+    image: SaLupaNgSarilingBayanBuongKuwentoCover,
+    link: "#buong-kuwento",
+    title: "Buong Kuwento",
+    description: "Tingnan ang detalyadong pangyayari.",
+  },
+  {
+    image: SaLupaNgSarilingBayanBuodCover,
+    link: "#buod",
+    title: "Buod",
+    description: "Isang maikling buod ng kwento.",
+  },
+  {
+    image: SaLupaNgSarilingKuwentongMayLarawanCover,
+    link: "#kuwentong-may-larawan",
+    title: "Kuwentong May Larawan",
+    description:
+      "Tingnan ang detalyadong pangyayari sa pamamagitan ng mga larawan.",
+  },
+];
+
+const quentinStories: Story[] = [
+  {
+    image: QuentinBuongKuwentoCover,
+    link: "#buong-kuwento",
+    title: "Buong Kuwento",
+    description: "Tingnan ang detalyadong pangyayari.",
+  },
+  {
+    image: QuentinBuodCover,
+    link: "#buod",
+    title: "Buod",
+    description: "Isang maikling buod ng kwento.",
+  },
+  {
+    image: QuentinKuwentongMayLarawanCover,
+    link: "#kuwentong-may-larawan",
+    title: "Kuwentong May Larawan",
+    description:
+      "Tingnan ang detalyadong pangyayari sa pamamagitan ng mga larawan.",
+  },
+];
+
 const emptyStories: Story[] = [
   {
     // image: BuongKuwento,
@@ -56,5 +137,11 @@ const emptyStories: Story[] = [
   },
 ];
 
-export { emptyStories, impengNegroStories };
+export {
+  emptyStories,
+  impengNegroStories,
+  quentinStories,
+  saLupaNgSarilingBayanStories,
+  tataSeloStories
+};
 

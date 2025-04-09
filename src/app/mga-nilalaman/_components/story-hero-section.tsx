30d8f4e1f0d852d741b651d0b702599f29e5c62e
xyugen <alex.arias.jorym@gmail.com>
2025-04-09 12:10:05 +0800

    fix: adjust tataselo cover to show face

--- src/app/mga-nilalaman/_components/story-hero-section.tsx
+++ src/app/mga-nilalaman/_components/story-hero-section.tsx
@@ -1,23 +1,26 @@
 import { cn } from "@/lib/utils";
+import React from "react";
 
 const StoryHeroSection = ({
   imageCover,
   title,
   className,
+  style,
 }: {
   imageCover: string;
   title: string;
   className?: string;
+  style?: React.CSSProperties;
 }) => {
   return (
     <div
       className={cn(
-        "relative flex h-96 flex-col items-center justify-center bg-cover bg-no-repeat text-surface-100",
+        "relative flex h-96 flex-col items-center justify-center bg-cover bg-no-repeat text-surface-100 bg-center",
         className
       )}
       style={{
         backgroundImage: `url(${imageCover})`,
-        backgroundPosition: "center",
+        ...style,
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-b from-stone-800/70 via-stone-800/55 to-stone-800/40"></div>
--- src/app/mga-nilalaman/tata-selo/layout.tsx
+++ src/app/mga-nilalaman/tata-selo/layout.tsx
@@ -6,12 +6,16 @@ import StoryHeroSection from "../_components/story-hero-section";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-      <HeroAnimation>
-        <LayoutSection>
-          <StoryHeroSection title="Tata Selo" imageCover={TataSeloCover.src} />
-          {children}
-        </LayoutSection>
-      </HeroAnimation>
+        <HeroAnimation>
+            <LayoutSection>
+                <StoryHeroSection
+                    title="Tata Selo"
+                    imageCover={TataSeloCover.src}
+                    className="bg-center md:bg-[center_top_-4rem] lg:bg-[center_top_-8rem]"
+                />
+                {children}
+            </LayoutSection>
+        </HeroAnimation>
     );
 };
 
